a3036a1226c9d59b20bd93fde858781859357206
zerglingf2a <zhaoyukun1999@gmail.com>
2022-09-22 20:08:59 -0300

    finished some exercises

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,23 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    String::from(input.to_string().trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut input = String::from(input);
+    input.push_str(" world!");
+    input
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let input = input.replace("cars", "balloons");
+    String::from(input)
 }
 
 #[cfg(test)]
